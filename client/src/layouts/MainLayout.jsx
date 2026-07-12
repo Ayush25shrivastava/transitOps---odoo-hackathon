ae6e4f70112c698d79e2bc09453d04f2069b5501
Ayush Shrivastava <ayushshrivastava2505@gmail.com>
2026-07-12 13:49:06 +0530

    add MainLayout component for improved layout structure and navigation

---
+++ client/src/layouts/MainLayout.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/common/Sidebar';
+import Navbar from '../components/common/Navbar';
+import { useApp } from '../context/AppContext';
+
+export default function MainLayout() {
+  const { sidebarOpen } = useApp();
+
+  return (
+    <div className="flex h-screen bg-[#0B0F19] overflow-hidden">
+      <Sidebar />
+      <div
+        className={`flex-1 flex flex-col overflow-hidden transition-all duration-300 ${
+          sidebarOpen ? 'ml-64' : 'ml-0'
+        }`}
+      >
+        <Navbar />
+        <main className="flex-1 overflow-y-auto p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
